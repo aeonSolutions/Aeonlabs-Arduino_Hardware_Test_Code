a49eeddbb690420647bda753ec6bb1e1e9704d10
Miguel Tomas Silva <mtpsilva@gmail.com>
2024-08-22 15:56:16 +0100

    code update I2C Scan

--- i2c_scan/i2c_scan.ino
+++ i2c_scan/i2c_scan.ino
@@ -19,7 +19,7 @@ void setup() {
   #if defined(CONFIG_IDF_TARGET_ESP32) 
     mySerial.begin(115200);
  #elif defined(CONFIG_IDF_TARGET_ESP32C3)
-    mySerial.begin(115200);
+    mySerial.begin(115200, SERIAL_8N1, 20, 21); // rxpin, txpin
  #elif defined(CONFIG_IDF_TARGET_ESP32S2)
     mySerial.begin(115200);
  #elif defined(CONFIG_IDF_TARGET_ESP32S3)
@@ -27,25 +27,57 @@ void setup() {
  #endif
 
 }
-
-void loop() {
   byte error, address;
   int nDevices = 0;
-
+void loop() {
+  nDevices = 0;
   delay(5000);
 
-  Serial.println("Scanning for I2C devices ...");
+  mySerial.println("Scanning for I2C devices ...");
   for(address = 0x01; address < 0x7f; address++){
     Wire.beginTransmission(address);
     error = Wire.endTransmission();
     if (error == 0){
-      Serial.printf("I2C device found at address 0x%02X\n", address);
+      if (deviceName(address) != "")
+        mySerial.println( deviceName(address) );
+
       nDevices++;
     } else if(error != 2){
-      Serial.printf("Error %d at address 0x%02X\n", error, address);
+      mySerial.printf("Error %d at address 0x%02X\n", error, address);
     }
   }
   if (nDevices == 0){
-    Serial.println("No I2C devices found");
+    mySerial.println("No I2C devices found");
+  }
+  mySerial.println("");
+}
+
+String deviceName(byte address){
+  switch(address) {
+    case 0x64:
+      return "0x64 -> ATSHA204a";
+      break;
+    case 0x6B:
+      return "0x6B -> LSM6DS3";
+      break;
+    case 0x48:
+      return "0x48 -> ADS1115";
+      break;
+    case 0x52:
+      return "0x52 -> ENS160";
+      break;
+    case 0x38:
+      return "0x38 -> AHT20";
+      break;
+    case 0x69:
+      return "0x69 -> RV-1805-C3";
+      break;
+    case 0x76:
+      return "0x76 -> BMP280";
+      break;
+    default:
+      mySerial.printf("I2C device found at address 0x%02X\n", address);
+      return "";
   }
 }
+
